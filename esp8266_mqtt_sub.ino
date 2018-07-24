1a7ae439e50804b91599a6ffe74f36c580ac35ef
Sujay Simha <37928088+SujaySimha81@users.noreply.github.com>
2018-07-24 23:32:26 +0530

    Add files via upload

---
+++ esp8266_mqtt_sub.ino
@@ -0,0 +1,87 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "redmi";
+const char* password = "prjk1234";
+const char* mqtt_server = "192.168.43.77";
+const char* topic = "tv";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX); //registering with random Id
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // ... and resubscribe
+      client.subscribe(topic);
+      client.subscribe("lala");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
---
+++ nodemcu_webserver.ino
@@ -0,0 +1,169 @@
+#include <ESP8266WiFi.h>
+ 
+const char* ssid = "Mohan";
+const char* password = "rekha123";
+ 
+int ledPin = D5; 
+int led2Pin = 5;
+WiFiServer server(80);
+
+
+
+void pic(){
+                                                                              
+Serial.println("                                    ```````");                                   
+Serial.println("                             ```..........-------.`");
+Serial.println("                         ``.....--..--------::://+/+/-`");                         
+Serial.println("                       `.---..........------:::://+++o+:.");                       
+Serial.println("                     .-----..........---------::///++oooo/.");                     
+Serial.println("                   .---:---...........----------::/+++ossso:`");                   
+Serial.println("                 `.-:::---...........-------:::::::+oossyyys+.");                  
+Serial.println("                `.:::::---.......-...-----:::::::///+osyyyyso+-");                 
+Serial.println("                .::::::---------------:::::::////+++osyyhhyysoo`");                
+Serial.println("                -::::::::------:::::::///////////++ossyyhhhyyso:");                
+Serial.println("               `-:::///::------::::///+++//::::://+oosyyyhhyyys/");                
+Serial.println("               .://///::--:::::::://:/++//:://++///+o+osyhyyyyy+.");               
+Serial.println("              `-:/++::--/+sossysssso/+++/::+osyyhyyyyyyssyhhhhyo:");               
+Serial.println("              `::+++::shhhhhyyyhhhyso+oo++syhhhhhhhhhhhhhyhhhhys/");               
+Serial.println("              `:/++/+hhhdhhhhhhhhhho+/::+yyddddddhhhhhysooshhhys/");               
+Serial.println("              `:/oo:ohdhhhhhhhdhhhso/:::ohyddmmmmdhyso+////yyhys-");               
+Serial.println("               -/o+:oyhhyysssyymmd++//-:+hhhdmmmdhhso+///:/hyhyo`");               
+Serial.println("               `/o/-/yhhhyysossddo//::yhyhhhhhdmdhys+///:-+hyyy+");                
+Serial.println("                :+:--/hhhhhhysoo+/-::shhyhhhhysyyssoo++/-:shyyy:");                
+Serial.println("               `--::--:+syyyso+:--:-odhhhhhhys+///o+os+//syhhso-");                
+Serial.println("               `::--::::/::///:::::/yhdddhhoyso++oossooosyyhhys:");                
+Serial.println("                ./:---/+/+ooo+/+/::yshdhddsoyyooysoso+ooosyhhy+");                 
+Serial.println("                 ./:-:///+++/++//:+hhmdydmhhsosysoo+++++oyhhy:");                  
+Serial.println("                  ./ooossso///+/:::sddhohddsssyssoossyyhhhhs/");                   
+Serial.println("                   -/o++yyyo::::::-::::/o+//oyyyysyhyyhddyy+`");                   
+Serial.println("                    :s+/s//++:::/:-----://+++sysyyh+--hdhy+`");                    
+Serial.println("                     .:/+o -+:::/::-:://::o/+/sshhh: ohhs-");                      
+Serial.println("                      -/:s:./://::/-/:/+//o/o/syyhy./hhho");                       
+Serial.println("                      .::+o:----./-.:`-/.-+-/::o++/+hhhy/");                       
+Serial.println("                      `::-/::---.:---.-:.-+-+++o+++yyyyy-");                       
+Serial.println("                      `::-:---::.-.:-:::://o/+oosyhysyys-");                       
+Serial.println("                      `//---:::::::/-+:+:o///ossyysoyhyy:");                       
+Serial.println("                       -/---::-:////////++++osyyssssyyho`");                       
+Serial.println("                        -/--::/-:///:::/+++ossssyhyyhyo`");                        
+Serial.println("                         .::-:::---:---/++++oo+syhyhy/`");                         
+Serial.println("                           .-///:-----:/++/:/+shhho-");                    
+Serial.println("                              .:++///+oso+++oshy/`");                      
+Serial.println("                                 `.-///::///:-`");                         
+
+
+}
+
+
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  pic();
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  
+  pinMode(led2Pin, OUTPUT);
+  digitalWrite(led2Pin, LOW);
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
+}
+int value = LOW;
+int value2 = LOW;
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+ 
+  // Match the request
+ 
+
+  if (request.indexOf("/LED=ON") != -1)  {
+    digitalWrite(ledPin, HIGH);
+    value = HIGH;
+  }
+  if (request.indexOf("/LED=OFF") != -1)  {
+    digitalWrite(ledPin, LOW);
+    value = LOW;
+  }
+  if (request.indexOf("/LED2=ON") != -1)  {
+    digitalWrite(led2Pin, HIGH);
+    value2 = HIGH;
+  }
+  if (request.indexOf("/LED2=OFF") != -1)  {
+    digitalWrite(led2Pin, LOW);
+    value2 = LOW;
+  }
+// Set ledPin according to the request
+//digitalWrite(ledPin, value);
+ 
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+ 
+  client.print("Led1 pin is now: ");
+ 
+  if(value == HIGH) {
+    client.print("On");
+  } else {
+    client.print("Off");
+  }
+  client.println("<br><br>");
+  client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");  
+
+   client.print("Led2 pin is now: ");
+ 
+  if(value2 == HIGH) {
+    client.print("On");
+  } else {
+    client.print("Off");
+  }
+  client.println("<br><br>");
+  client.println("<a href=\"/LED2=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/LED2=OFF\"\"><button>Turn Off </button></a><br />");  
+  client.println("</html>");
+ 
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+}
+
